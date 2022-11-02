317ca8363419a26913fd1938d4438dd6b984a3c9
Vimal Pandey <107113571+vimalpandey041@users.noreply.github.com>
2022-11-02 14:57:03 +0530

    new

---
+++ script.js
@@ -0,0 +1,20 @@
+// swiper js 
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
